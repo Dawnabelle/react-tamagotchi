b7d64707705449b95b2b1fae2cebe1cf67582ecf
dawn mort and kristen chellis <kristen@>
2018-07-05 08:38:21 -0700

    add handle functions to pet component

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 
-function Pet(){
+class Pet extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hunger = 10;
+      boredom= 10;
+      energy = 10;
+    }
+  }
+  handleFeed(){
+    this.setState({hunger: 10})
+  }
+  handleSleep(){
+    this.setState({energy: 10})
+  }
+  handlePlay(){
+    this.setState({boredom: 10})
+  }
 
   return(
     <div>
